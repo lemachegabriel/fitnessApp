46fb3b61c3802d8ec83720443e4c94c74767f7e9
lemachegabriel <lemache35@gmail.com>
2022-03-14 10:48:27 -0300

    Initial Commit

---
+++ fitnessApp/fitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitnessAppApp.swift
+//  fitnessApp
+//
+//  Created by Gabriel Lemache Castro on 14/03/22.
+//
+
+import SwiftUI
+
+@main
+struct fitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
